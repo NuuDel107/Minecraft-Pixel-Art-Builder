4b3a38b788247b18d98dd707bc1ba07e245cdc66
NuuDel107 <75998901+NuuDel107@users.noreply.github.com>
2021-07-04 22:52:36 +0000

    Made a very unoptimized prototype of the color-to-block converter

--- app.js
+++ app.js
@@ -1,14 +1,43 @@
 const fs = require("fs");
 
-const testValue = [30, 100, 30]
+const testValue = [63, 31, 24];
+
+const colorTemplate = ["R", "G", "B"];
 
 var colordata = fs.readFileSync("colors.json");
-
 colordata = JSON.parse(colordata);
 
+console.log("JSON parsed");
+var blockColors = [];
 
-var blocks = Object.keys(colordata.blocks);
+var blockKeys = Object.keys(colordata.blocks);
 
-blocks.forEach(key => {
-    console.log(colordata.blocks[key].R);
+blockKeys.forEach(key => {
+    blockColors.push(colordata.blocks[key]);
 });
+
+
+var distances = [];
+
+blockColors.forEach(colors => {
+    
+    let totalDistance = 0;
+
+    for (let i = 0; i <= 2; i++)
+    {
+        totalDistance += Math.abs(colors[colorTemplate[i]] - testValue[i]);
+    }
+    
+    distances.push(totalDistance);
+    
+})
+
+function indexOfSmallest(a) {
+ var lowest = 0;
+ for (var i = 1; i < a.length; i++) {
+  if (a[i] < a[lowest]) lowest = i;
+ }
+ return lowest;
+}
+
+console.log(blockKeys[indexOfSmallest(distances)])
