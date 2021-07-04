5d11ef305c298f07fdf88115f45c2b93cfaa5cb0
nuudeli <>
2021-07-04 21:28:41 +0000

    Added a test RGB value

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const fs = require("fs");
 
+const testValue = [30, 100, 30]
+
 var colordata = fs.readFileSync("colors.json");
 
 colordata = JSON.parse(colordata);
 
-var blocks = Object.keys(colordata.blocks)
+
+var blocks = Object.keys(colordata.blocks);
 
 blocks.forEach(key => {
-    console.log(colordata.blocks[key].R)
+    console.log(colordata.blocks[key].R);
 });
