6ccd82e3fdaf925cb896c85c0e9335be65df6a40
NuuDel107 <nuudeli.productions@gmail.com>
2021-07-05 15:27:06 +0300

    Added WSS for MC, listens to command and prints testing image in world

--- app.js
+++ app.js
@@ -1,12 +1,36 @@
-const BlockConverter = require("./blockConverter/blockConverter");
-const GetPixels = require("./image-processing/getPixels");
+const WebSocket = require("ws")
+const fs = require("fs")
 
-const bConverter = new BlockConverter();
+const Pixels = require("./pixels");
+const JSONSender = require("./jsonSender");
+
+const wss = new WebSocket.Server({ port: 80 })
 
 
-GetPixels.getPixels(120, 120, pixels => {
-    
-    pixels.forEach(colors => {
-        console.log(bConverter.colorToBlock([colors.r, colors.g, colors.b]))
+wss.on("connection", ws => {
+    JSONSender.subscribe(ws, "PlayerMessage")
+
+    ws.on("message", packet => {
+        const data = JSON.parse(packet);
+
+        if(data.body.eventName == "PlayerMessage")
+        {
+            const message = data.body.properties.Message;
+
+            if(message.substring(0, 6) == "!print")
+            {
+                fs.writeFileSync("log.txt", " ");
+                Pixels.placePixels(200, 200, async commands => {
+                    for (var i = 0; i < commands.length; i++) {
+
+                        JSONSender.sendCommand(ws, commands[i]);
+                        console.log(commands[i]);
+                        fs.writeFileSync("log.txt", commands[i] + "\n", {flag: "a+"}); 
+                        await new Promise(resolve => setTimeout(resolve, 1));
+                        
+                    };
+                });
+            }
+        }
     });
 });
