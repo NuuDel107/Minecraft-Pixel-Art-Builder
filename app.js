bc1a046ef55ff3042c6f1ec8de2e979be235ab37
NuuDel107 <nuudeli.productions@gmail.com>
2021-07-05 11:53:55 +0300

    Currently has only a quick testing script for converter

--- app.js
+++ app.js
@@ -1,43 +1,8 @@
 const fs = require("fs");
+const BlockConverter = require("./convert/blockConverter");
 
-const testValue = [63, 31, 24];
+const testValue = [3, 187, 69];
+const bConverter = new BlockConverter();
 
-const colorTemplate = ["R", "G", "B"];
+console.log(bConverter.colorToBlock(testValue))
 
-var colordata = fs.readFileSync("colors.json");
-colordata = JSON.parse(colordata);
-
-console.log("JSON parsed");
-var blockColors = [];
-
-var blockKeys = Object.keys(colordata.blocks);
-
-blockKeys.forEach(key => {
-    blockColors.push(colordata.blocks[key]);
-});
-
-
-var distances = [];
-
-blockColors.forEach(colors => {
-    
-    let totalDistance = 0;
-
-    for (let i = 0; i <= 2; i++)
-    {
-        totalDistance += Math.abs(colors[colorTemplate[i]] - testValue[i]);
-    }
-    
-    distances.push(totalDistance);
-    
-})
-
-function indexOfSmallest(a) {
- var lowest = 0;
- for (var i = 1; i < a.length; i++) {
-  if (a[i] < a[lowest]) lowest = i;
- }
- return lowest;
-}
-
-console.log(blockKeys[indexOfSmallest(distances)])
