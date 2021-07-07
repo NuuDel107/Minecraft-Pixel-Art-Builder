5835af97693fd41f2c012f60f95c7b9a5039263d
NuuDel107 <nuudeli.productions@gmail.com>
2021-07-07 21:40:49 +0300

    Added check for working image link

--- app.js
+++ app.js
@@ -13,6 +13,9 @@ const wss = new WebSocket.Server({ port: 80 })
 wss.on("connection", ws => {
     JSONSender.subscribe(ws, "PlayerMessage")
 
+    JSONSender.say(ws, "Connected to Pixel Art Builder");
+    JSONSender.say(ws, "For more information, type !print help");
+
     ws.on("message", packet => {
         const data = JSON.parse(packet);
 
@@ -32,33 +35,39 @@ wss.on("connection", ws => {
                 console.log(uri, width, height);
 
                 request.head(uri, (err, res, body) => {
-                    if (imageTypes.includes(res.headers['content-type']))
+                    if(res != undefined)
                     {
+                        if (imageTypes.includes(res.headers['content-type']))
+                        {
 
-                        request(uri).pipe(fs.createWriteStream("image.png")).on("close", () => {
+                            request(uri).pipe(fs.createWriteStream("image.png")).on("close", () => {
 
-                            fs.writeFileSync("log.txt", " ");
+                                fs.writeFileSync("log.txt", " ");
 
 
 
-                            Pixels.getPixels(width, height, async commands => {
-                                for (var i = 0; i < commands.length; i++) {
-            
-                                    JSONSender.sendCommand(ws, commands[i]);
-                                    console.log(commands[i]);
-            
-                                    fs.writeFileSync("log.txt", commands[i] + "\n", {flag: "a+"}); 
-            
-                                    await new Promise(resolve => setTimeout(resolve, 1));
-                                    
-                                };
+                                Pixels.getPixels(width, height, async commands => {
+                                    for (var i = 0; i < commands.length; i++) {
+                
+                                        JSONSender.sendCommand(ws, commands[i]);
+                                        console.log(commands[i]);
+                
+                                        fs.writeFileSync("log.txt", commands[i] + "\n", {flag: "a+"}); 
+                
+                                        await new Promise(resolve => setTimeout(resolve, 1));
+                                        
+                                    };
+                                });
+
                             });
-
-                        });
+                        }
+                        
+                        else {
+                            JSONSender.say(ws, "ERROR: Image must be of type 'jpeg', 'png', 'bmp' or 'gif'");
+                        }
                     }
-                    
                     else {
-                        JSONSender.sendCommand(ws, "/say ERROR: Image must be of type 'jpeg', 'png', 'bmp' or 'gif'");
+                        JSONSender.say(ws, "ERROR: Image not found (check link spelling?)");
                     }
                 });
 
